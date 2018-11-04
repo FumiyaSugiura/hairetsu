a2f256305ba859eddf52d464ee21e996abb22ddf
fumiya-lit <kussan628.fumi1027@icloud.com>
2018-11-04 13:53:45 +0900

    first commit

--- 配列/ViewController.swift
+++ 配列/ViewController.swift
@@ -10,6 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var itsuLabel: UILabel!
+    @IBOutlet var dokodeLabel: UILabel!
+    @IBOutlet var daregaLabel: UILabel!
+    @IBOutlet var naniwoLabel: UILabel!
+    @IBOutlet var doushitaLabel: UILabel!
+    
+    let itsuArray: [String] = ["1年前","１週間前","昨日","今日"]
+    let dokodeArray: [String] = ["山の上で","アメリカで","学校で","生徒会室で"]
+    let daregaArray: [String] = ["会長が","先生が","総理が","友達が"]
+    let naniwoArray: [String] = ["PCを","紙を","Wii Uを","Wi-Fiを"]
+    let doushitaArray: [String] = ["壊した","無駄にした","食べた","踏んだ"]
+    
+    var itsu: Int!
+    var dokode: Int!
+    var darega: Int!
+    var naniwo: Int!
+    var doushita: Int!
+    
+    @IBAction func change(){
+        itsu = Int(arc4random_uniform(4))
+        dokode = Int(arc4random_uniform(4))
+        darega = Int(arc4random_uniform(4))
+        naniwo = Int(arc4random_uniform(4))
+        doushita = Int(arc4random_uniform(4))
+        
+        itsuLabel.text = itsuArray[itsu]
+        dokodeLabel.text = dokodeArray[dokode]
+        daregaLabel.text = daregaArray[darega]
+        naniwoLabel.text = naniwoArray[naniwo]
+        doushitaLabel.text = doushitaArray[doushita]
+        
+    }
+    
+    @IBAction func reset(){
+        itsuLabel.text = " --- "
+        dokodeLabel.text = " --- "
+        daregaLabel.text = " --- "
+        naniwoLabel.text = " --- "
+        doushitaLabel.text = " --- "
+    }
+    
+    @IBAction func random(){
+        itsu = Int(arc4random_uniform(5))
+        dokode = Int(arc4random_uniform(5))
+        darega = Int(arc4random_uniform(5))
+        naniwo = Int(arc4random_uniform(5))
+        doushita = Int(arc4random_uniform(5))
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
